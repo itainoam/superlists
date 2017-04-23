52d3a32972e72d9d943c9df88ba8d102f7c32617
Itai Noam <itai.noam@safebreach.com>
2017-04-23 17:24:36 +0300

    adds unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,20 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox();
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+  
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+  def tearDown(self):
+    self.browser.quit()
+  def test_can_start_a_list_and_retrieve_it_later(self):
+    # edith opens the todo homepage
+    self.browser.get('http://localhost:8000')
+    
+    # she notices the page and title header mentions to-do lists
+    self.assertIn('To-Do', self.browser.title)
+    self.fail('Finish the test!')
+§
+
+if __name__ == '__main__':
+    unittest.main()
