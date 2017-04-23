1079887950d809f95ae2d1f89e9bfb49d107de0f
Itai Noam <itai.noam@safebreach.com>
2017-04-23 22:13:21 +0300

    functional test to check for input

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,6 +15,27 @@ class NewVisitorTest(unittest.TestCase):
     
     # she notices the page and title header mentions to-do lists
     self.assertIn('To-Do', self.browser.title)
+    header_text = self.browser.find_element_by_tag_name('h1').text
+
+    # she is invited to enter a todo
+    inputbox = self.browser.find_element_by_id('ud')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
+    # she types "Buy feathers" into a text box
+    inputbox.send_keys('Buy feathers')
+
+    #when she hits enter, the page updaates with the item as #1
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_elements_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: Buy feathers' for row in rows)
+    )
+
     self.fail('Finish the test!')
 
 
