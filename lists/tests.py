dd98ebeaec9d4b4086d7bf63f1878f7a468401a2
Itai Noam <itai.noam@safebreach.com>
2017-04-25 11:51:34 +0300

    rename home page tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
-class test_uses_home_template(TestCase):
-  def test_home_page_returns_correct_html(self):
+class HomePageTest(TestCase):
+  def test_root_url_resolves_to_home_page_view(self):
     response = self.client.get('/')
     self.assertTemplateUsed(response, 'home.html')
   
