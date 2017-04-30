f817cdab1eec6e73f76bd9c1186549db48569004
Itai Noam <itai.noam@safebreach.com>
2017-04-30 10:51:41 +0300

    replace the use of wait with a test tries until success or timeout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,18 +1,29 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
   
   def setUp(self):
     self.browser = webdriver.Firefox()
 
-  def check_for_row_in_list_table(self,row_text):
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn(row_text, [row.text for row in rows])
-
+  def wait_for_row_in_list_table(self,row_text):
+    start_time = time.time()
+    while True:
+      try:
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('row_text', [row.text for row in rows])
+        return
+      except (AssertionError, WebDriverException) as e:
+        if time.time() - start_time > MAX_WAIT:
+          raise e
+        time.sleep(0.5)
+  
   def tearDown(self):
     self.browser.quit()
     
@@ -36,20 +47,18 @@ class NewVisitorTest(LiveServerTestCase):
 
     #when she hits enter, the page updates with the item as #1
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
 
-    self.check_for_row_in_list_table('1.Buy feathers')
+    self.wait_for_row_in_list_table('1.Buy feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
-    time.sleep(1)
 
     # The page updates again, and now shows both items on her list
-    self.check_for_row_in_list_table('1.Buy feathers')
-    self.check_for_row_in_list_table('2.Use peacock feathers to make a fly')
+    self.wait_for_row_in_list_table('1.Buy feathers')
+    self.wait_for_row_in_list_table('2.Use peacock feathers to make a fly')
 
     # Edith wonders whether the site will remember her list. Then she sees
     # that the site has generated a unique URL for her -- there is some
