35346bda3bd1a80e163cb278bcedcc38e151ce6c
Itai Noam <itai.noam@safebreach.com>
2017-04-30 08:59:57 +0300

    make functional test use builtin django testing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
   
   def setUp(self):
     self.browser = webdriver.Firefox()
@@ -18,7 +18,7 @@ class NewVisitorTest(unittest.TestCase):
     
   def test_can_start_a_list_and_retrieve_it_later(self):
     # edith opens the todo homepage
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
     
     # she notices the page and title header mentions to-do lists
     self.assertIn('To-Do', self.browser.title)
@@ -57,7 +57,3 @@ class NewVisitorTest(unittest.TestCase):
     self.fail('Finish the test!')
 
     # She visits that url - her to do list is still there
-
-
-if __name__ == '__main__':
-    unittest.main()
